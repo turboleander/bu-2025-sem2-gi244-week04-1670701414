5130248e37a82fffdb1f176ebd9538ab764ef24d
turboleander <53143665+turboleander@users.noreply.github.com>
2026-02-06 10:52:52 +0700

    Week4

--- Assets/Scripts/DestroyOutOfBound.cs
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -5,6 +5,9 @@ public class DestroyOutOfBound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.z > 30 || transform.position.z < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] enemyPrefabs;
+    public GameObject[] animalPrefabs;
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            float PosSpawnIndex = Random.Range(-10f,10f);
+            if (animalPrefabs[animalIndex] != null)
+            {
+                Instantiate(animalPrefabs[animalIndex], new Vector3(PosSpawnIndex, 0, 20), Quaternion.Euler(0, 180, 0));
+            }
+            else
+            {
+                Debug.LogWarning($"null prefab at index {animalIndex}");
+            }
+        }
     }
 }
